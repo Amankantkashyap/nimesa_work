9b18eff60fa4bec30a225fbe9e9d0d2cb2c5a4d1
akant <Akant641@gmail.com>
2020-08-11 21:59:10 +0530

    new annotation addes

--- RestApiLearning/src/main/java/restservice/model/Product.java
+++ RestApiLearning/src/main/java/restservice/model/Product.java
@@ -1,7 +1,11 @@
 package restservice.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@AllArgsConstructor
+@NoArgsConstructor
 @Data
 public class Product {
     private int id;
